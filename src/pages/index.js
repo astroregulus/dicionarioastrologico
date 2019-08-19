8da3e2fca04a75a870860ed780cc5d13b40ea023
Arthur Fücher <arthurfucher@gmail.com>
2019-08-19 14:26:20 -0300

    adiciona link para letras na página principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Dicionário Astrológico</h1>
+
+    <div style={{
+          display: "flex",
+          flexFlow: "column"
+        }}>
+      <Link to="/a">A</Link>
+      <Link to="/c">C</Link>
+    </div>
+
   </Layout>
 )
 

bf0813f5707b3e07ec4975b956cde2c2cb6e1750
Arthur Fücher <arthurfucher@gmail.com>
2019-08-20 18:44:32 -0300

    adicionado google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146095738-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

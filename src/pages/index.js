5054f7969c3a262327ad107b5cc066244f9d2fa7
Arthur Fücher <arthurfucher@gmail.com>
2019-08-21 17:42:20 -0300

    Altera estilo do menu de letras na pagina inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,32 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from 'gatsby'
 
+const linkStyle = {
+  textDecoration: "none",
+  color:"white",
+  width:"48px",
+  height:"48px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#011830",
+  margin: "5px"
+}
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Dicionário Astrológico</h1>
+    <p>Olá! Aqui você encontrará diversos termos astrológicos e seus significados/explicações.</p>
+
+    <p>Clique na letra que deseja:</p>
 
     <div style={{
           display: "flex",
-          flexFlow: "column"
+          flexFlow: "row",
         }}>
-      <Link to="/a">A</Link>
-      <Link to="/c">C</Link>
+        <Link to="/a" style={linkStyle}>A</Link>
+        <Link to="/c" style={linkStyle}>C</Link>
+
     </div>
 
   </Layout>

8939d30d6ced0751c4018191eb1b005d91e48f84
Arthur Fücher <arthurfucher@gmail.com>
2019-08-22 17:59:47 -0300

    cria pagina de bibliografia

---
+++ src/pages/bibliografia.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from '../components/layout'
+import SEO from '../components/seo';
+
+const BibliografiaPage = () => (
+    <Layout>
+        <SEO title={"Bibliografia"}/>
+        <h2>Bibliografia</h2>
+        <h3>Livros</h3>
+        <a href="http://www.livrariadaregulus.com.br/produto/palavraschave-da-astrologia-118" 
+            target="_blank"
+            rel="noopener noreferrer">Palavras Chave da Astrologia</a>
+
+    </Layout>
+)
+
+export default BibliografiaPage;

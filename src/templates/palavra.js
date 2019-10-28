507ed3bc3a9bf3debe4ece4e13986b0a045443f1
Arthur Fücher <arthurfucher@gmail.com>
2019-10-28 13:54:55 -0300

    adiciona link para letra na pagina da palavra

--- src/templates/palavra.js
+++ src/templates/palavra.js
@@ -1,13 +1,16 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Layout from '../components/layout'
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
+  const path = frontmatter.path.split("/");
+
   return (
     <Layout>
+      <Link to={path[1]}>{path[1].toUpperCase()}</Link>
       <div className="blog-post-container">
         <div className="blog-post">
           <h1>{frontmatter.title}</h1>
